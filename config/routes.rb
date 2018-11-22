f69931d6efdc0df947ad5505472b624d5b3e4cbe
Francis Thometz <francis.thometz@gmail.com>
2018-11-22 15:28:05 +0900

    Created Cocktails Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
