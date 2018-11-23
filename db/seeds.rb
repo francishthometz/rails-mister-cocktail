369e40d8cae80f15d32f1e0cfd1505378a7744ba
Francis Thometz <francis.thometz@gmail.com>
2018-11-23 17:29:52 +0900

    Fixed Mister Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ puts 'Create ingredients'
 url1 = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients = JSON.parse(open(url1).read)
 ingredients['drinks'].each do |ingredient|
-  i = Ingredient.create(name: ingredient['strIngredient'])
+  i = Ingredient.create(name: ingredient['strIngredient1'])
   puts "create #{i.name}"
 end
 
